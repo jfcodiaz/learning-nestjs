57defc5665f35295b076599a9855a2e04d9e594f
JFcoDiaz <jfcodiaz@gmail.com>
2022-11-27 14:38:02 -0600

    09 create services and repostiory

--- app/02-messages/src/messages/messages.controller.ts
+++ app/02-messages/src/messages/messages.controller.ts
@@ -1,20 +1,27 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { createMessageDto } from './dtos/create-nessage.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
-  @Get()
-  listMessages(){
+  messagesService: MessagesService;
 
+  constructor() {
+    this.messagesService = new MessagesService();
+  }
+
+  @Get()
+  listMessages() {
+    return this.messagesService.findAll();
   }
 
   @Post()
   createMessage(@Body() body:createMessageDto) {
-    console.log(body);
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
   getMessage(@Param('id') id:string) {
-    console.log(id);
+    return this.messagesService.findOne(id);
   }
 }
---
+++ app/02-messages/src/messages/messages.repository.ts
@@ -0,0 +1,32 @@
+import {writeFile, readFile} from 'fs/promises';
+
+export class MessagesRepository {
+  constructor() {
+    
+  }
+
+  async findOne(id: string) {
+    const content = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(content);
+
+    return messages[id];
+  }
+
+  async findAll() {
+    const content = await readFile('messages.json', 'utf-8')
+    const messages = JSON.parse(content);
+
+    return messages;
+  }
+
+  async create(content: string) {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents);
+
+    const id = Math.floor(Math.random() * 999);
+
+    messages[id] = { id, content };
+
+    await writeFile('messages.json', JSON.stringify(messages));
+  }
+}
