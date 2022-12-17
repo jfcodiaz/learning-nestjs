8c956bcc6026ceca42662f62015466b2bd4f9d44
JFcoDiaz <jfcodiaz@gmail.com>
2022-12-17 02:09:01 -0600

    19 Complete user CRUD

--- app/04-mycv/src/users/users.controller.ts
+++ app/04-mycv/src/users/users.controller.ts
@@ -1,5 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { 
+  Body,
+  Controller,
+  Post,
+  Get,
+  Patch,
+  Delete,
+  Param,
+  Query,
+  NotFoundException
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user';
+import { UpdateUserDto } from './dtos/update-user';
 import { UsersService } from './users.service';
 
 @Controller('auth')
@@ -7,10 +18,31 @@ export class UsersController {
   constructor(private userService:UsersService){}
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
-    this.userService.create(
+    return this.userService.create(
       body.email,
       body.password
     );
-    console.log(body);
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    const user = this.userService.findOne(parseInt(id));
+    return user ?? new NotFoundException('');
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email:string) {
+    return this.userService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id:string) {
+    return this.userService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body:UpdateUserDto) {
+    return this.userService.update(parseInt(id), body)
+
   }
 }
